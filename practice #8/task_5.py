76cc173249990e09d5d198ca5283c3e9588eacf6
kahaderaz <vvinebrand@gmail.com>
2024-11-12 09:43:31 +0700

    task_5

---
+++ practice #8/task_5.py
@@ -0,0 +1,28 @@
+class RealString:
+    # инициализация объекта класса с заданной строкой
+    def __init__(self, value):
+        self.value = value  # сохраняем переданное значение как атрибут объекта
+
+    # метод для получения длины строки
+    def __len__(self):
+        return len(self.value)  # возвращаем количество символов в строке
+
+    # метод для сравнения строк по количеству символов
+    def __lt__(self, other):
+        if isinstance(other, RealString):  # проверяем, является ли другой объект экземпляром RealString
+            return len(self) < len(other)  # сравниваем длину текущей строки с длиной другой строки
+        elif isinstance(other, str):  # проверяем, является ли другой объект обычной строкой
+            return len(self) < len(other)  # сравниваем длину текущей строки с длиной обычной строки
+
+        return NotImplemented  # если объект не того типа, возвращаем NotImplemented
+
+    # метод для вывода строки
+    def __str__(self):
+        return self.value  # возвращаем значение строки для удобного отображения
+
+real_str1 = RealString("Apple")
+real_str2 = RealString("Яблоко")
+
+print(real_str1 < real_str2)
+
+print(real_str1 < "Banana")
